3d69714f0fb6dcbd43324935335bc68bee8b56f3
Jagdish Mehra <jagdishmehra961@gmail.com>
2024-07-20 15:18:19 +0530

    Updated file upload and added shorten url

--- RecordVideoAndUploadUtils.py
+++ RecordVideoAndUploadUtils.py
@@ -10,10 +10,12 @@ import os
 CONFIG_PATH = "./config.json"
 fp = open(CONFIG_PATH)
 config = json.load(fp)
+bayId = config["bayId"]
 base_url_tech = config["baseUrlTech"]
-file_upload_url_tech = base_url_tech + "/fileStorage/upload/s3"
+file_upload_url_tech = base_url_tech + "/fileStorage/upload"
 file_download_endpoint = "/fileStorage/download/"
 ECON_CAMERA_CONFIG_PATH = "./CameraParameters/EconCameraConfig.json"
+ANIMATIONS_DIR_PATH = os.path.join(bayId, str(config["animationDirectory"]))
 
 
 class PlayerVideoInfo:
@@ -51,8 +53,7 @@ def recordVideo(videoFolderPath):
 
 def recordVideoUsingNetworkCameraWithLogo(videoFolderPath, logoPath, rtspUrl, videoLength):
     # Ensure the video folder exists
-    if not os.path.exists(os.path.dirname(videoFolderPath)):
-        os.makedirs(os.path.dirname(videoFolderPath))
+    create_folder_if_not_exists(videoFolderPath)
 
     logo = cv2.imread(logoPath, cv2.IMREAD_UNCHANGED)
     video = cv2.VideoCapture(rtspUrl)
@@ -111,7 +112,7 @@ def uploadVideo(videoPath, videoName):
         if response.status_code == 200:
             fileId = response.json()["id"]
             fileUrl = base_url_tech + file_download_endpoint + fileId
-            return str(fileUrl)
+            return shortenUrl(fileUrl)
         return None
     except Exception as e:
         print(e)
@@ -129,13 +130,16 @@ def checkForFile(filePath):
 
 def transferVideoToAllPlayersVideosFolder(all_players_dir_path, current_video_full_path,
                                           current_video_file_name, current_player, current_ball_score):
+    create_folder_if_not_exists(all_players_dir_path)
     copyAndPasteVideo(current_video_full_path, all_players_dir_path)
-    os.rename(all_players_dir_path + current_video_file_name,
+    add_animation_to_video(all_players_dir_path + "/" + current_video_file_name, current_ball_score)
+    os.rename(all_players_dir_path + "/" + current_video_file_name,
               all_players_dir_path + "/" + current_player + "_" + current_ball_score + ".avi")
 
 
 def checkIfBetterShot(all_players_dir_path, current_video_full_path,
                       current_video_file_name, current_player, current_ball_score):
+    create_folder_if_not_exists(all_players_dir_path)
     file_for_player_exists = False
     for filename in os.listdir(all_players_dir_path):
         name = filename.split(".")[0]
@@ -156,17 +160,96 @@ def checkIfBetterShot(all_players_dir_path, current_video_full_path,
 
 
 def getAllPlayerVideoUrls(all_players_dir_path):
+    create_folder_if_not_exists(all_players_dir_path)
     allVideoIds = []
     for filename in os.listdir(all_players_dir_path):
-        videoUrl = uploadVideo(all_players_dir_path + filename, filename)
+        videoUrl = uploadVideo(all_players_dir_path + "/" + filename, filename)
         playerVideoInfo = {"playerId": filename.split("_")[1], "videoUrl": videoUrl}
         allVideoIds.append(playerVideoInfo)
 
         # delete file after uploading because the game is ended.
-        deleteFile(all_players_dir_path + filename)
+        # deleteFile(all_players_dir_path + "/" + filename)
 
     return allVideoIds
 
 
+def create_folder_if_not_exists(folder_path):
+    # Ensure the directory exists or create it if it doesn't
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
 def deleteFile(filePath):
     os.remove(filePath)
+
+
+def add_animation_to_video(video_path, score):
+    for filename in os.listdir(ANIMATIONS_DIR_PATH):
+        animation_of_score = filename.split(".")[0]
+        if animation_of_score == score:
+            merge_videos(video_path, ANIMATIONS_DIR_PATH + "/" + filename, video_path)
+            break
+
+
+def merge_videos(video1_path, video2_path, output_path):
+    # Open the first video file
+    video1 = cv2.VideoCapture(video1_path)
+    if not video1.isOpened():
+        print(f"Error: Could not open video file '{video1_path}'")
+        return
+
+    # Open the second video file
+    video2 = cv2.VideoCapture(video2_path)
+    if not video2.isOpened():
+        print(f"Error: Could not open video file '{video2_path}'")
+        return
+
+    # Get video properties (assuming both videos have the same properties)
+    fps = video1.get(cv2.CAP_PROP_FPS)
+    width = int(video1.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video1.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    out = get_video_writer(full_path=output_path, dimensions=(width, height), fps=fps)
+
+    # Read and write frames from video1
+    while True:
+        ret, frame = video1.read()
+        if ret:
+            out.write(frame)
+        else:
+            break
+
+    # Read and write frames from video2
+    while True:
+        ret, frame = video2.read()
+        if ret:
+            out.write(frame)
+        else:
+            break
+
+    # Release resources
+    video1.release()
+    video2.release()
+    out.release()
+
+
+def shortenUrl(url):
+    payload = {
+        "url": url
+    }
+
+    # Necessary to get a non-html response
+    headers = {
+        "Accept": "application/json"
+    }
+
+    response = requests.post("https://spoo.me", data=payload, headers=headers)
+
+    if response.status_code == 200:
+        # If the request was successful, print the shortened URL
+        return response.json().get("short_url")
+    else:
+        # If the request failed, print the error message
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return url
