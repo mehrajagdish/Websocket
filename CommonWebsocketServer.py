48348d1f6c523034685366da5ebc8970ed966788
Jagdish Mehra <jagdishmehra961@gmail.com>
2024-08-13 12:17:34 +0530

    WS server: close gracefully in case of keyboard interrupt

--- CommonWebsocketServer.py
+++ CommonWebsocketServer.py
@@ -38,4 +38,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Server shutting down gracefully")
